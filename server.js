35c51c8069004b5d7df8be571fd471649a706188
cristianl <crisian.lombardi@gmail.com>
2020-03-18 14:48:36 -0300

    Primer Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict'
+
+// Agregar las variables de .env a las variables de entorno
+require('dotenv').config({ path: require('path').join(__dirname, '.env') });
+
+const
+  express = require('express'),
+  path = require('path'),
+  port = process.env.PORT || 8080,
+  app = express(),
+  mailController = require('./server/controllers/mail'),
+  bodyParser = require('body-parser');
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('./build/'));
+
+// Router para la API que envía mail
+const routerMail = express.Router();
+routerMail.route('/mail').post(mailController.postMail);
+app.use('/api', routerMail);
+
+// Router para lo estatico
+const router = express.Router();
+router.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './build/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+});
+app.use(router);
+
+app.listen(port);
+
+console.log("Variable de entorno de prueba", process.env.REACT_APP_UAI);
+console.log("Variable de entorno de prueba", process.env.REACT_APP_API_URL);
+console.log('Servidor corriendo en puerto: ' + port);
